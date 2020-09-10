c930f5a920c0f5362613b4cced22fc580567d65f
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-09-10 12:58:48 +0300

    Menu update

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,48 +1,78 @@
-import React, { FunctionComponent, useState, Fragment } from 'react';
+import React, { FunctionComponent, useState, Fragment, useEffect } from "react";
 
-import MenuLauncher from './MenuLauncher';
-import MenuPop from './MenuPop';
-import { MenuListContent } from './MenuPop/MenuPop';
+import MenuLauncher from "./MenuLauncher";
+import MenuPop, { MenuListContent } from "./MenuPop/MenuPop";
+import MenuPopMobile from "./MenuPopMobile";
 
 const Menu: FunctionComponent<MenuProps> = ({
-    label,
-    text,
-    menuListContent,
-    commands,
-    text2,
+  label,
+  text,
+  menuListContent,
+  commands,
+  text2,
 }: MenuProps) => {
-    const [menuState, setMenuState] = useState(false);
+  const [menuState, setMenuState] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    const menuStateToggle: VoidFunction = () => {
-        setMenuState(!menuState);
-    };
-    console.log(window.innerWidth);
-    return (
-        <Fragment>
-            <MenuLauncher
-                onClick={menuStateToggle}
-                active={menuState}
-                label={label}
-            />
-            <MenuPop
-                label={label}
-                visible={menuState}
-                text={text}
-                menuListContent={menuListContent}
-                commands={commands}
-                text2={text2}
-                onClick={menuStateToggle}
-            />
-        </Fragment>
-    );
+  useEffect(() => {
+      if (window.innerWidth <= 1200) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+  }, [])
+
+  const menuStateToggle: VoidFunction = () => {
+    setMenuState(!menuState);
+  };
+
+  const mobileCLoseHandler = () => {
+    if (isMobile) {
+      return setMenuState(false);
+    } else {
+      return () => {};
+    }
+  };
+
+  return (
+    <Fragment>
+      <MenuLauncher
+        onClick={menuStateToggle}
+        active={menuState}
+        label={label}
+      />
+      {isMobile ? (
+        <MenuPopMobile
+          mobileClose={mobileCLoseHandler}
+          label={label}
+          visible={menuState}
+          text={text}
+          menuListContent={menuListContent}
+          commands={commands}
+          text2={text2}
+          onClick={menuStateToggle}
+        />
+      ) : (
+        <MenuPop
+          label={label}
+          visible={menuState}
+          text={text}
+          menuListContent={menuListContent}
+          commands={commands}
+          text2={text2}
+          onClick={menuStateToggle}
+        />
+      )}
+    </Fragment>
+  );
 };
 
 interface MenuProps {
-    label: string;
-    text?: string | boolean;
-    menuListContent?: Array<MenuListContent>;
-    commands?: string;
-    text2?: string;
+  label: string;
+  text?: string | boolean;
+  menuListContent?: Array<MenuListContent>;
+  commands?: string;
+  text2?: string;
 }
 
 export default Menu;
