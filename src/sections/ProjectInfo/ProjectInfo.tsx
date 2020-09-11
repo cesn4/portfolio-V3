275841fc24ce687f603335dc1604016f18e1b651
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-09-11 11:20:52 +0300

    layout fix

--- src/sections/ProjectInfo/ProjectInfo.tsx
+++ src/sections/ProjectInfo/ProjectInfo.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, Fragment } from "react";
-import { Row, Col } from "react-grid-system";
+import { Container, Row, Col } from "react-grid-system";
 
 import { ProjectInfoObject } from "~/mocks/projects";
 
@@ -10,54 +10,58 @@ const ProjectInfo: FunctionComponent<ProjectInfoProps> = ({
 }: ProjectInfoProps) => {
   const className = "project-info";
   return (
-    <Fragment>
-      <Row>
-        <Col lg={12} xl={12} md={12}>
-          <span className={`${className}__title`}>{project.title}</span>
-        </Col>
-      </Row>
-      <Row>
-      <Col lg={6} xl={6} md={6}>
-          <div className={`${className}__image-box`}>
-            <img
-              src={project.image}
-              alt=""
-              className={`${className}__main-image`}
-            ></img>
-            <div className={`${className}__secondary-image-box`}>
-              <img
-                className={`${className}__image`}
-                src={project.imageL}
-                alt=""
-              ></img>
-              <img
-                className={`${className}__image`}
-                src={project.imageR}
-                alt=""
-              ></img>
-            </div>
-          </div>
-        </Col>
-      <Col lg={6} xl={6} md={6}>
-          <div className={`${className}__info-box`}>
-            <div className={`${className}__summary`}>
-              <span className={`${className}__label`}>Summary</span>
-              <span className={`${className}__text`}>{project.summary}</span>
-            </div>
-            <div className={`${className}__summary`}>
-              <span className={`${className}__label`}>Tools</span>
-              <span className={`${className}__text`}>{project.tools}</span>
-            </div>
-            <div className={`${className}__summary`}>
-              <span className={`${className}__label`}>Link</span>
-              <a className={`${className}__text`} href={project.link}>
-                {project.link}
-              </a>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Fragment>
+      <div className={`${className}`}>
+        <Container>
+          <Row>
+            <Col lg={12} xl={12} md={12}>
+              <span className={`${className}__title`}>{project.title}</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={6} xl={6} md={6}>
+              <div className={`${className}__image-box`}>
+                <img
+                  src={project.image}
+                  alt=""
+                  className={`${className}__main-image`}
+                ></img>
+                <div className={`${className}__secondary-image-box`}>
+                  <img
+                    className={`${className}__image`}
+                    src={project.imageL}
+                    alt=""
+                  ></img>
+                  <img
+                    className={`${className}__image`}
+                    src={project.imageR}
+                    alt=""
+                  ></img>
+                </div>
+              </div>
+            </Col>
+            <Col lg={6} xl={6} md={6}>
+              <div className={`${className}__info-box`}>
+                <div className={`${className}__summary`}>
+                  <span className={`${className}__label`}>Summary</span>
+                  <span className={`${className}__text`}>
+                    {project.summary}
+                  </span>
+                </div>
+                <div className={`${className}__summary`}>
+                  <span className={`${className}__label`}>Tools</span>
+                  <span className={`${className}__text`}>{project.tools}</span>
+                </div>
+                <div className={`${className}__summary`}>
+                  <span className={`${className}__label`}>Link</span>
+                  <a className={`${className}__text`} href={project.link}>
+                    {project.link}
+                  </a>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
   );
 };
 
