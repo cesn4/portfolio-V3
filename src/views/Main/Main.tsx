a8d07a8f897fe9d71f9f1180b6056fc7faa77001
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-09-12 13:20:53 +0300

    Update Main.tsx

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -13,7 +13,7 @@ const Main: FunctionComponent = () => {
     <div className={`${className}`}>
       <MainLayout>
         <motion.div
-          initial={{ opacity: 0}}
+          initial={{ opacity: 1}}
           animate={{ opacity: 1}}
           exit={{ opacity: 0}}
           transition={{ delay: 0.2, duration: 0.1}}
