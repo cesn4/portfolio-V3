4eec5cf08642fad8709612e0257f274d2cd00517
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-09-21 13:18:10 +0300

    update

--- src/mocks/documentation.ts
+++ src/mocks/documentation.ts
@@ -1,6 +1,6 @@
 export const documentation: DocumentationObject = {
   text:
-  "Welcome to AI mc49! It's an application created to represent Front-End web-developer Mindaugas Cesna. All possible AI responses can be found in Commands section.",
+  "Welcome to AI mc49! It's an application created to represent Front-End Web developer Mindaugas Cesna. All possible AI responses can be found in Commands section.",
 };
 
 interface DocumentationObject {
